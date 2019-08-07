357105f16e591cfa728a575032fbb6b63e0e96f0
JRA2880 <44944337+JRA2880@users.noreply.github.com>
2019-08-06 23:55:46 -0700

    UPdate Files to GitHub
    Update Basic Python Files to GitHub

---
+++ commentDocString.py
@@ -0,0 +1,18 @@
+
+# This is a practice file
+
+"""This is my note
+and this is still my note
+and this again is still my note.
+"""
+
+##This is my line of code
+
+
+def printMe():
+    """This is my docstring
+    """
+
+    print("Meeeeeee!")
+
+print(printMe._doc_)
